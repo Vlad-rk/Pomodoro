0d5fb369a66755a3f696b2f38c0b6ce9313d1422
Vladimir Rekai <vladimir.rekai@etu.unicaen.fr>
2023-09-17 14:19:10 +0200

    ListenerButton

--- js/script.js
+++ js/script.js
@@ -1,10 +1,34 @@
 const timerText = document.getElementById("timerText")
+const startResetButton = document.getElementById("startResetButton")
+let chosenTime = 1500
 let time = 1500
 let start = false
 
 setInterval(changeTime, 1000)
 
+startResetButton.addEventListener('click',()=>{
+    if(start){
+        start = false
+        resetTimer()
+        startResetButton.textContent = "Commencer"
+    }else{
+        start = true
+        startResetButton.textContent = "Réinitialiser"
+    }
+});
+
 function changeTime(){
-    time--
+    if(start){
+        time--
+        setTimerText()
+    }
+}
+
+function resetTimer(){
+    time = chosenTime
+    setTimerText()
+}
+
+function setTimerText(){
     timerText.textContent = time
 }
